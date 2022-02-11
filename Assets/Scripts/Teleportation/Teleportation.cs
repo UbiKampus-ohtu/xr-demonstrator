bb7cfe74e0f9888e7e6855d3888e81acf39e2af7
sainigma <sainigma@gmail.com>
2022-02-11 10:35:49 +0200

    updated teleportation to work with hand grab event

--- Assets/Scripts/Teleportation/Teleportation.cs
+++ Assets/Scripts/Teleportation/Teleportation.cs
@@ -1,64 +1,98 @@
 ﻿using UnityEngine;
 
 public class Teleportation : MonoBehaviour {
-	private Camera _camera;
-	[SerializeField] private GameObject playerCharacter;
-	[SerializeField] private GameObject teleportationSphere;
-	private GameObject _sphere;
+  private Camera _camera;
+  private GameObject playerCharacter;
+  private CharacterController controller;
+  public GameObject teleportationSphere;
+  private GameObject sphere;
 
-	[SerializeField] private MovementActions mv;
-	private bool teleportationEnabled = false;
-	private Vector3 direction = Vector3.zero;
-	
-	void Start() {
-		_camera = Camera.main;
-	}
+  private MovementActions mv;
+  private Vector3 deltaPosition = Vector3.zero;
 
-	void Awake() {
-		mv = new MovementActions();
-	}
+  private bool initialized = false;
 
-	void OnEnable() {
-		mv.Enable();
-	}
+  void Awake() {
+    mv = new MovementActions();
+  }
 
-	private bool keyPressed = false;
-	void Update() {
-		if (!keyPressed && mv.Player.teleport.ReadValue<float>() > 0) {
-			keyPressed = true;
-			HandleTeleportationCircle();
-		} else if (keyPressed && mv.Player.teleport.ReadValue<float>() <= 0) {
-			keyPressed = false;
-		}
+  void OnEnable() {
+    mv.Enable();
+  }
 
-		if (teleportationEnabled) {
-			var point = new Vector3(_camera.pixelWidth / 2.0f, _camera.pixelHeight / 2.0f, 0 );
-			Ray ray = _camera.ScreenPointToRay(point);
-			if (Physics.Raycast(ray, out var hit)) {
-				if (hit.transform.CompareTag("Floor")) {
-					direction = hit.point - ray.origin;
-					_sphere.transform.position = hit.point;
-				}
-			}
+  private bool keyPressed = false;
+  void Update() {
+    if (!keyPressed && mv.Player.teleport.ReadValue<float>() >= 0.5f) {
+      keyPressed = true;
+      SpawnTeleportationCircle();
+    } else if (keyPressed && mv.Player.teleport.ReadValue<float>() < 0.5f) {
+      keyPressed = false;
+      HandleTeleportation();
+    }
 
-			if (mv.Player.click.ReadValue<float>() > 0 ) {
-				playerCharacter.GetComponent<CharacterController>().Move(direction);
-				teleportationEnabled = false;
-				Destroy(_sphere);
-				direction = Vector3.zero;
-			}
-		}			
-	}
+    if (keyPressed) {
+      UpdateTeleportationCircle();
+    } else {
+      return;
+    }
 
-	private void HandleTeleportationCircle() {
-		if (!teleportationEnabled) {
-			teleportationEnabled = keyPressed =  true;
-			_sphere = Instantiate(teleportationSphere) as GameObject;
-		} else {
-			teleportationEnabled = false;
-			Destroy(_sphere);
-			direction = Vector3.zero;
-		} 
+    if (mv.Player.click.ReadValue<float>() > 0) {
+      HandleTeleportation();
+    }
+  }
 
-	}
+  private void SpawnTeleportationCircle() {
+    sphere = Instantiate(teleportationSphere);
+  }
+
+  private void UpdateTeleportationCircle() {
+    if (sphere == null) return;
+    
+    LayerMask mask = LayerMask.GetMask("Default");
+    float distance = 10f;
+
+    Ray ray = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
+    if (Physics.Raycast(ray, out var hit, distance, mask)) {
+      if (hit.transform.CompareTag("Floor")) {
+        deltaPosition = hit.point - ray.origin;
+        sphere.transform.position = hit.point;
+      } else {
+        sphere.transform.position = hit.point;
+        //Muuta teleportaatiospawner ruksiksi tjsp
+      }
+    }
+  }
+
+  private void HandleTeleportation() {
+    Init();
+    if (controller != null) {
+      controller.Move(deltaPosition);
+      deltaPosition = Vector3.zero;
+    }
+    CollectTeleportationCircle();
+  }
+
+  private void CollectTeleportationCircle() {
+    Destroy(sphere);
+    deltaPosition = Vector3.zero;
+  }
+
+  private void Init() {
+    if (initialized) return;
+
+    GameObject cursor = gameObject;
+    while (controller == null) {
+      controller = cursor.GetComponent<CharacterController>();
+      if (cursor.transform.parent == null) {
+        break;
+      } else {
+        cursor = cursor.transform.parent.gameObject;
+      }
+    }
+
+    if (controller == null) {
+      Destroy(gameObject);
+    }
+    initialized = true;
+  }
 }
