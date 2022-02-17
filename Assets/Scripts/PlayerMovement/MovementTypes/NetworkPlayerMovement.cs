ee3db3c18a647634d9d324a962bdf9dca488916a
sainigma <sainigma@gmail.com>
2022-02-17 17:24:13 +0200

    rewrote networkmanager to read fixed messages. networkmanager events now also use generic instead of string as a parameter. modified networkplayermovement to reflect the changes

--- Assets/Scripts/PlayerMovement/MovementTypes/NetworkPlayerMovement.cs
+++ Assets/Scripts/PlayerMovement/MovementTypes/NetworkPlayerMovement.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 
 public class NetworkPlayerMovement : PlayerMovement {
   private Vector3 playerPosition = Vector3.zero;
-  private Vector3 playerRotation = Vector3.zero;
+  private Quaternion playerRotation = Quaternion.identity;
 
   public override void Start() {
     base.Start();
     movement.gravity = false;
-    NetworkManager.startListening("player id_here", doSomething);
+    NetworkManager.startListening("player id_here 0 transform", updateTransform);
+    //NetworkManager.startListening("player id_here 1 transform", updateRightHand);
+    //NetworkManager.startListening("player id_here 2 transform", updateHead);
+    //NetworkManager.startListening("player id_here 3 transform", updateLeftHand);
   }
 
-  private void doSomething(string payload) {
-    MovementPayload movementPayload = PlayerSerializations.JsonStringToSerialization(payload);
-    playerPosition = movementPayload.pos;
+  private void updateTransform(object payload) {
+    float [] transform = (float [])payload;
+    playerPosition = new Vector3(transform[0], transform[1], transform[2]);
+    playerRotation = new Quaternion(transform[3], transform[4], transform[5], transform[6]);
   }
 
   private void Update() {
