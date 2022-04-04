4b5f2bbdf79c606daad70146094c6147b41ab7b5
sainigma <sainigma@gmail.com>
2022-04-04 15:12:53 +0300

    fixed tests. began refactoring project so that it compiles to multiple assembly definitions. defs ok for Inputs (old ActionMaps) and Management (msg brokers from utils)

--- Assets/Tests/Playmode/LocalPlayerTest.cs
+++ Assets/Tests/Playmode/LocalPlayerTest.cs
@@ -14,15 +14,24 @@ namespace Tests {
     }
 
     [UnityTest]
-    public IEnumerator LocalPlayerGravityWorks() {
-      GameObject player = GameObject.Find("LocalPlayer");
+    public IEnumerator PlayerGravityWorks() {
+      yield return new WaitForSeconds(0.5f);
+      GameObject player = GameObject.Find("Player [connId=0]");
 
       Vector3 initialPosition = player.transform.position;
       float originalDistanceToOrigo = initialPosition.magnitude;
-      yield return new WaitForSeconds(0.5f);
+      yield return new WaitForSeconds(1f);
       float distanceToOrigo = player.transform.position.magnitude;
       Assert.Less(distanceToOrigo, originalDistanceToOrigo);
-      Assert.Less(distanceToOrigo, 1f);
+      Assert.Less(distanceToOrigo, 0.5f);
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerTurningWorks() {
+      yield return new WaitForSeconds(0.5f);
+      GameObject player = GameObject.Find("Player [connId=0]");
+      Vector3 originalForward = player.transform.forward;
+      
     }
   }
 }
