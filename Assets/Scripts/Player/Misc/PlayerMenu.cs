6869ded19f7d37e4f7bf38add3899ab7be445a37
sainigma <sainigma@gmail.com>
2022-04-14 16:47:12 +0300

    menu can be toggled

--- Assets/Scripts/Player/Misc/PlayerMenu.cs
+++ Assets/Scripts/Player/Misc/PlayerMenu.cs
@@ -5,12 +5,21 @@ using Mirror;
 
 public class PlayerMenu : MonoBehaviour {
   private GameObject menuComponents;
+  private MovementActions mv;
+  private DebouncableTrigger menuButton = new DebouncableTrigger(0.1f, 0.9f);
+  private bool menuActive = false;
+
   private void Start() {
     menuComponents = transform.Find("MenuComponents").gameObject;
-    menuComponents.SetActive(false);
+    menuComponents.SetActive(menuActive);
+  }
+
+  void Awake() {
+    mv = new MovementActions();
   }
 
   private void OnEnable() {
+    mv.Enable();
     EventManager.startListening("player menu", buttonPressed);
   }
 
@@ -20,5 +29,16 @@ public class PlayerMenu : MonoBehaviour {
 
   public void buttonPressed(string param) {
     Debug.Log("Hei Maailma!");
+  }
+
+  private void Update() {
+    float menuPressed = mv.Player.menu.ReadValue<float>();
+
+    Debug.Log(menuPressed);
+
+    if (menuButton.update(menuPressed) && menuButton.get()) {
+      menuActive = !menuActive;
+      menuComponents.SetActive(menuActive);
+    }
   }
 }
