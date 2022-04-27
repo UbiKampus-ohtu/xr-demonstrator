c89997d6846f093a42df3f11f45083a63a2c39a6
sainigma <sainigma@gmail.com>
2022-04-27 14:19:24 +0300

    removed unused code in MQTTManager, began reworking on data parsing

--- Assets/Scripts/Managers/MQTTManager.cs
+++ Assets/Scripts/Managers/MQTTManager.cs
@@ -58,76 +58,9 @@ public class MQTTManager : MonoBehaviour {
     return new IPEndPoint(addressList[0], port);
   }
 
-  private int sendGeneralMessageSegment(byte[] data, int cursor) {
-    string target = "";
-    string payload = "";
-    bool targetParsed = false;
-    bool finished = false;
-
-    while (cursor < data.Length && !targetParsed) {
-      if (data[cursor] == '\n') {
-        targetParsed = true;
-      } else {
-        target += (char)data[cursor];
-      }
-      cursor++;
-    }
-
-    while (cursor < data.Length - 1 && !finished) {
-      if (data[cursor] == '\n') {
-        trigger(target, payload);
-
-        if (data[cursor + 1] == '\n') {
-          finished = true;
-          cursor++;
-        } else {
-          payload = "";
-        }
-
-      } else {
-        payload += (char)data[cursor];
-      }
-      cursor++;
-    }
-
-    return cursor;
-  }
-
-  private void parseGeneralMessage(byte[] data) {
-    int cursor = 1;
-    while (cursor < data.Length) {
-      cursor = sendGeneralMessageSegment(data, cursor);
-    }
-  }
-
-  private float [] parsePlayerTransform(byte[] data, int cursor) {
-    float [] transform = new float[7];
-    Buffer.BlockCopy(data, cursor, transform, 0, 7 * 4);
-    Debug.LogFormat("{0} {1} {2} {3} {4} {5} {6}", transform[0], transform[1], transform[2], transform[3], transform[4], transform[5], transform[6]);
-    return transform;
-  }
-
-  private void parsePlayerData(byte[] data) {
-    int cursor = 1;
-    int previousPlayer = -1;
-    int messageNumber = 0;
-
-    while (cursor < data.Length) {
-      int playerId = data[cursor];
-      float [] transform = parsePlayerTransform(data, cursor + 1);
-
-      if (playerId != previousPlayer) {
-        messageNumber = 0;
-        previousPlayer = playerId;
-      } else {
-        messageNumber++;
-      }
-
-      string eventId = String.Format("player {0} {1} transform", playerId, messageNumber);
-      trigger(eventId, transform[0]);
-
-      cursor += 7 * 4;
-    }
+  private void parseMessage(byte[] data) {
+    if (data.Length == 0) return;
+    //datan pyörittely tähän, loppuun triggerit
   }
 
   private void onMessage(IAsyncResult message) {
@@ -138,16 +71,7 @@ public class MQTTManager : MonoBehaviour {
 
     udpClient.BeginReceive(new AsyncCallback(onMessage), udpClient);
 
-    //Debug.LogFormat("Received {0} bytes", data.Length);
-
-    if (data.Length == 0) {
-      return;
-    }
-    if (data[0] == 1) {
-      parseGeneralMessage(data);
-    } else if (data[0] == 2) {
-      parsePlayerData(data);
-    }
+    parseMessage(data);
   }
 
   public static void startListening(string eventName, Action<object> listener) {
